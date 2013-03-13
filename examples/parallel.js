610c380b66181bc74570b034b046027c95f5d726
TJ Holowaychuk <tj@vision-media.ca>
2013-03-13 11:41:04 -0700

    add parallel example

---
+++ examples/parallel.js
@@ -0,0 +1,23 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Batch = require('..');
+
+var n = 10;
+var batch = new Batch;
+
+while (n--) {
+  (function(n){
+    batch.push(function(done){
+      console.log('  start : %s', n);
+      setTimeout(function(){
+        console.log('   done : %s', n);
+        done();
+      }, 200);
+    })
+  })(n);
+}
+
+batch.end();
